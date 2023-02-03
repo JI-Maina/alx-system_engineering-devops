eeec01c93db53288a55bd82d33488353e5bb4979
JI-Maina <irungujmaina@gmail.com>
2023-02-03 10:19:05 +0300

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 from = ARGV[0].scan(/from:(.\w+)/)
-to = ARGV[0].scan(/to:([+\w+]+)/)
+to = ARGV[0].scan(/to:(.\w+)/)
 flags = ARGV[0].scan(/flags:(.{12})/)
 
-puts ARGV[0].scan(from, to, flags).join(',')
+puts [from, to, flags].join(',')
