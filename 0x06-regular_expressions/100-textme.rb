8be9a692a7a66d228aace9f0264296fe559a71b9
JI-Maina <irungujmaina@gmail.com>
2023-02-03 10:02:25 +0300

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:(.\w+)/).scan(/to:([+\w+]+)/).scan(/flags:(.{12}))/).join(',')
+puts ARGV[0].scan(/from:(.\w+)/).scan(/to:([+\w+]+)/).scan(/flags:(.{12})/).join(',')
