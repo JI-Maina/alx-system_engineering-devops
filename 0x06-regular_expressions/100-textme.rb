3f9945f212ce9511d76e8a8061e0c034ac9a291d
JI-Maina <irungujmaina@gmail.com>
2023-02-02 23:13:07 -0800

    text me

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,4 +3,4 @@ from = ARGV[0].scan(/from:(.\w+)/)
 to = ARGV[0].scan(/to:([+\w+]+)/)
 flags = ARGV[0].scan(/flags:(.{12})/)
 
-puts ARGV[0].scan(from, to, flags).join(',')
+puts [from, to, flags].join(',')
