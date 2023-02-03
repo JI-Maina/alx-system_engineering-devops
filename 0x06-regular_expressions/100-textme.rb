1e52053a1f44ce9250b09e3d4a37a2543a981d2b
JI-Maina <irungujmaina@gmail.com>
2023-02-03 10:36:08 +0300

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-from = ARGV[0].scan(/from:(.\w+)/)
-to = ARGV[0].scan(/to:(.\w+)/)
-flags = ARGV[0].scan(/flags:(.{12})/)
+from = ARGV[0].scan(/from:(.\w+)\]/)
+to = ARGV[0].scan(/to:(.\w+)\]/)
+flags = ARGV[0].scan(/flags:(.{12})\]/)
 
 puts [from, to, flags].join(',')
