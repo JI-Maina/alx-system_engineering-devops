20b9605dd3d76740217313590d9ad709731e2cc3
JI-Maina <irungujmaina@gmail.com>
2023-04-18 10:43:47 -0700

    Quite an experience to live in fear, isn't it?

---
+++ 0x14-mysql/create_replica_user.sql
@@ -0,0 +1,4 @@
+-- create a new user for the replica server
+CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'root';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
