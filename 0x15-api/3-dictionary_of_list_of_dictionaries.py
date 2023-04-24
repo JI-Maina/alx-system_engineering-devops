fc23a0bbf75da775944129ed7eff71b752495ef8
JI-Maina <irungujmaina@gmail.com>
2023-04-24 13:20:40 -0700

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Export all user data in the JSON format
+
+Format: { "USER_ID": [ 
+    {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS},
+    {"username": "USERNAME", "task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS},
+    ... ],
+    "USER_ID": [ ... ] }
+
+File name must be: todo_all_employees.json
+"""
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+
+    for user in users:
+        for todo in todos:
+            if todo['userId'] == user['id']:
+                with open('todo_all_employees.json', 'w') as f:
+                    json.dump({user['id']: [{
+                        'username': user.get('username'),
+                        'task': todo.get('title'),
+                        'completed': todo.get('completed')
+                        } for todo in todos ]}, f)
+    
+    #print(todos)
+    #print(users)
