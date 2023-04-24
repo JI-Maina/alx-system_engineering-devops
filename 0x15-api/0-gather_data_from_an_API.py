43ad51e671ecc6292b659f7d80cbda8163d3a329
JI-Maina <irungujmaina@gmail.com>
2023-04-24 08:47:40 -0700

    pycode correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,9 +3,8 @@
 This module provides a script that given an employee ID, returns information
 about his/her TODO list progress.
 """
-
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     pay = {'userId': argv[1]}
