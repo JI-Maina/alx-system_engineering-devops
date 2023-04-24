f580d43af6102eff6595d0e542d510d3289d9bdf
JI-Maina <irungujmaina@gmail.com>
2023-04-24 14:22:24 +0000

    first commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """given employee ID, returns information about his/her TODO list progress"""
 
-import requests
 from sys import argv
+import requests
 
-payload = {'userId': argv[1]}
-payload1 = {'id': argv[1]}
+if __name__ == "__main__":
+    pay = {'userId': argv[1]}
+    pay1 = {'id': argv[1]}
 
-r = requests.get('https://jsonplaceholder.typicode.com/todos', params=payload)
-u = requests.get('https://jsonplaceholder.typicode.com/users', params=payload1)
+    r = requests.get('https://jsonplaceholder.typicode.com/todos', params=pay)
+    u = requests.get('https://jsonplaceholder.typicode.com/users', params=pay1)
 
-user = u.json()
-todos = r.json()
+    user = u.json()
+    todos = r.json()
 
-complete = 0
-for todo in todos:
-    if todo["completed"] == True:
-        complete = complete + 1
+    complete = 0
+    for todo in todos:
+        if todo["completed"] == True:
+            complete = complete + 1
 
-print("Employee {} is done with tasks({}/{}):"
-        .format(user[0]['name'], complete, len(todos)))
+    print("Employee {} is done with tasks({}/{}):"
+            .format(user[0]['name'], complete, len(todos)))
 
-for todo in todos:
-    if todo["completed"] == True:
-        print('     ' + todo['title'])
+    for todo in todos:
+        if todo["completed"] == True:
+            print('     ' + todo['title'])
